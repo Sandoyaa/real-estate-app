87c3ce38c782a85dc79ff4482ff67c12991b8a06
sandoya <sergeysurzhikov2@gmail.com>
2026-02-18 22:53:18 +0300

    Initial commit

---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const Signin = () => {
+  return (
+    <View>
+      <Text>Signin</Text>
+    </View>
+  );
+};
+
+export default Signin;
+
